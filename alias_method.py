33c03e982c2ba6476dc64999ec07adceaef8a6c4
litiancheng <litiancheng@360.cn>
2022-08-16 15:28:05 +0800

    alias

--- alias_method.py
+++ alias_method.py
@@ -24,20 +24,29 @@ class Alias(object):
             high_index = high_nums.pop()
             high_prob = dic[token2id[high_index]]
 
+            area[low_index] = low_prob
+            alias[low_index] = high_index
+
             diff = 1.0 / n - low_prob
             remain = high_prob - diff
             dic[token2id[high_index]] = remain
-            area[low_index] = low_prob
-            alias[low_index] = high_index
             if remain > 1.0 / n:
                 high_nums.append(high_index)
             else:
                 low_nums.append(high_index)
+        #可能会有正好等于1/n的还没pop出来
+        while high_nums:
+            high_index = high_nums.pop()
+            area[high_index] = 1.0 / n
+        while low_nums:
+            low_index = low_nums.pop()
+            area[low_index] = 1.0 / n
+
         return area, alias, n, token2id
 
     def sample(self):
         num1 = random.randint(0,self.n)
-        num2 = random.random()
+        num2 = random.uniform(0,1.0/self.n)
         if num2 < self.area[num1]:
             return self.token2id[num1]
         else:
